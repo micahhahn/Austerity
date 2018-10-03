ff480f57a288708fb9faa96c497e5b60027b1505
Micah Hahn <micah.s.hahn@gmail.com>
2018-10-03 01:52:01 -0400

    Add statement model and validation

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DataKinds #-}
+
 module Lib
     ( someFunc
     ) where
@@ -8,8 +10,11 @@ import Pdf.Document.Internal.Types
 import Pdf.Document.PageNode
 import Pdf.Content.Processor
 
+import Money
+
+import Data.Int
 import Data.List (sortBy, groupBy)
-import Data.HashMap.Strict
+import Data.Text (Text)
 import qualified Data.Text as T
 import Text.Groom
 import Data.Maybe (catMaybes)
@@ -27,13 +32,74 @@ data TextSpan = TextSpan { _sLeft :: Int
                          , _sText :: T.Text 
                          } deriving (Show)
 
+data Date = Date !Int16 !Int8 !Int8
+            deriving (Show, Eq, Ord)
+
+newtype Money = Money { unMoney :: Double }
+                deriving (Show)
+
+data Statement = Statement 
+    { fromDate :: Date
+    , toDate :: Date
+    , accountNumber :: Text
+    , accountName :: Text
+    , summary :: Summary
+    , transactions :: [Transaction]
+    } deriving (Show)
+
+data Summary = Summary
+    { beginningBalance :: Dense "USD"
+    , endingBalance :: Dense "USD"
+    , depositsCount :: Int
+    , depositsSum :: Dense "USD"
+    , withdrawalsCount :: Int
+    , withdrawalsSum :: Dense "USD"
+    } deriving (Show)
+
+data Amount = Deposit (Dense "USD")
+            | Withdrawal (Dense "USD")
+            deriving (Show)
+
+data Transaction = Transaction
+    { id :: Text
+    , date :: Date
+    , postDate :: Date
+    , description :: Text
+    , amount :: Amount
+    , balance :: Dense "USD"
+    } deriving (Show)
+
+{- Relatively simple validation to make sure the statement is being processed correctly -}
+validateStatement :: Statement -> Maybe String
+validateStatement s
+    | toDate s >= fromDate s = Just $ "fromDate " ++ show (fromDate s) ++ " should be before toDate " ++ show (toDate s)
+    | depositsCount (summary s) /= length deposits = Just $ "Expected " ++ show (depositsCount (summary s)) ++ " deposits, found " ++ show (length deposits)
+    | depositsSum (summary s) /= foldr (+) (dense' 0.0) deposits = Just $ "Expected " ++ show (depositsSum (summary s)) ++ " of deposits, found " ++ show (foldr (+) (dense' 0.0) deposits) 
+    | withdrawalsCount (summary s) /= length withdrawals = Just $ "Expected " ++ show (withdrawalsCount (summary s)) ++ " withdrawals, found " ++ show (length withdrawals)
+    | withdrawalsSum (summary s) /= foldr (+) (dense' 0.0) withdrawals = Just $ "Expected " ++ show (withdrawalsSum (summary s)) ++ " of withdrawls, found " ++ show (foldr (+) (dense' 0.0) deposits)
+    | endingBalance (summary s) /= (balance . last . transactions) s = Just $ "Expected ending balance of " ++ show (endingBalance (summary s)) ++ " , found " ++ show ((balance . last . transactions) s)
+    | otherwise = case foldr mapper (Right $ beginningBalance . summary $ s) (transactions s) of
+                       Left s -> Just s
+                       _ -> Nothing
+    where deposits = [ x | (Deposit x) <- amount <$> transactions s]
+          withdrawals = [ x | (Withdrawal x) <- amount <$> transactions s]
+
+          flattenAmount (Deposit x) = x
+          flattenAmount (Withdrawal x) = -x
+
+          mapper :: Transaction -> Either String (Dense "USD") -> Either String (Dense "USD")
+          mapper _ (Left s) = Left s
+          mapper t (Right a) = if a + flattenAmount (amount t) == balance t
+                                   then Right $ balance t
+                                   else Left $ "Expected ending balance of " ++ show (a + flattenAmount (amount t)) ++ ", but found " ++ show (balance t)
+
 flattenGlyph :: Span -> TextBox
 flattenGlyph (Span gs _) = 
     let (Vector left top) = (glyphTopLeft . head) gs
         (Vector right bottom) = (glyphBottomRight . last) gs
         text = T.concat . catMaybes $ glyphText <$> gs
     in TextBox (floor left) (floor top) (floor bottom) (floor right) text
-
+ 
 sortNodes :: TextBox -> TextBox -> Ordering
 sortNodes (TextBox l1 t1 r1 b1 text1) (TextBox l2 t2 r2 b2 text2) = case compare t1 t2 of
     EQ -> case compare l1 l2 of
