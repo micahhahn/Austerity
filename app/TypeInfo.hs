7d5a8fb68664d523a124c56865fc5f2d9ed2ac86
micahhahn <micah.s.hahn@gmail.com>
2019-02-11 14:07:02 -0600

    Handler recursive data types in makeTypeInfo

--- app/TypeInfo.hs
+++ app/TypeInfo.hs
@@ -7,8 +7,6 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
 
-{-# LANGUAGE DeriveGeneric #-}
-
 module TypeInfo (
     makeTypeInfo,
     DatatypeInfo(..),
@@ -19,19 +17,16 @@ module TypeInfo (
 
 import Data.Map.Lazy (Map)
 import qualified Data.Map.Lazy as Map
+import Data.Set (Set)
+import qualified Data.Set as Set
 import Data.Text (Text)
 import qualified Data.Text as Text
 import Data.Time
 import Data.Typeable
 import GHC.Generics
 
-import Control.Exception
-
-data X = X [Int]
-    deriving (Generic, Typeable)
-
 makeTypeInfo :: forall t. (Typeable t, Generic t, GDatatypeInfo (Rep t)) => Proxy t -> Map TypeRep DatatypeInfo
-makeTypeInfo t = getDatatypeInfo (from (undefined :: t)) (typeRep (Proxy :: Proxy t))
+makeTypeInfo t = getDatatypeInfo (from (undefined :: t)) (typeRep t) Set.empty
 
 data DatatypeInfo = DatatypeInfo 
     { _datatypeName :: Text
@@ -63,60 +58,56 @@ type family (IsPrim a) :: Bool where
     IsPrim Double    = 'True
     IsPrim a         = 'False
 
-x = Map.insert 1 1 Map.empty
-y = Map.insert 1 undefined Map.empty
-
 class MkTypeInfo (p :: Bool) a where
-    mkTypeInfo :: Proxy p -> Proxy a -> Map TypeRep DatatypeInfo
+    mkTypeInfo :: Proxy p -> Proxy a -> Set TypeRep -> Map TypeRep DatatypeInfo
 
 instance MkTypeInfo 'True a where
-    mkTypeInfo _ _ = Map.empty
+    mkTypeInfo _ _ _ = Map.empty
 
 instance (Typeable t, Generic t, GDatatypeInfo (Rep t)) => MkTypeInfo 'False t where
-    mkTypeInfo _ p = makeTypeInfo p
+    mkTypeInfo _ p ts = getDatatypeInfo (from (undefined :: t)) (typeRep p) ts
 
 class GDatatypeInfo f where
-    getDatatypeInfo :: f a -> TypeRep -> Map TypeRep DatatypeInfo
+    getDatatypeInfo :: f a -> TypeRep -> Set TypeRep -> Map TypeRep DatatypeInfo
 
 instance (Datatype a, GConstructorInfo c) => GDatatypeInfo (D1 a c) where
-    getDatatypeInfo c@(M1 r) t = let (cs, m') = getConstructorInfo r
-                                     m = Map.insert t (DatatypeInfo (Text.pack . datatypeName $ c) (Text.pack . moduleName $ c) (Text.pack . packageName $ c) (isNewtype c) cs) Map.empty
-                                  in Map.union m m'
+    getDatatypeInfo c@(M1 r) t ts = let (cs, m') = getConstructorInfo r (Set.insert t ts)
+                                        m = Map.insert t (DatatypeInfo (Text.pack . datatypeName $ c) (Text.pack . moduleName $ c) (Text.pack . packageName $ c) (isNewtype c) cs) Map.empty
+                                     in Map.union m m'
 
 class GConstructorInfo f where
-    getConstructorInfo :: f a -> ([ConstructorInfo], Map TypeRep DatatypeInfo)
+    getConstructorInfo :: f a -> Set TypeRep -> ([ConstructorInfo], Map TypeRep DatatypeInfo)
 
 instance (Constructor a, GSelectorInfo c) => GConstructorInfo (C1 a c) where
-    getConstructorInfo c@(M1 r) = let (t, m) = getSelectorInfo r
-                                    in ([ConstructorInfo (Text.pack . conName $ c) (conFixity c) (conIsRecord c) t], m)
+    getConstructorInfo c@(M1 r) ts = let (t, m) = getSelectorInfo r ts
+                                      in ([ConstructorInfo (Text.pack . conName $ c) (conFixity c) (conIsRecord c) t], m)
 
 instance (GConstructorInfo a, GConstructorInfo b) => GConstructorInfo (a :+: b) where
-    getConstructorInfo (_ :: (a :+: b) f) = let (lc, lm) = getConstructorInfo (undefined :: a f)
-                                                (rc, rm) = getConstructorInfo (undefined :: b f)
-                                             in (lc ++ rc, Map.union lm rm)
+    getConstructorInfo (_ :: (a :+: b) f) ts = let (lc, lm) = getConstructorInfo (undefined :: a f) ts
+                                                   (rc, rm) = getConstructorInfo (undefined :: b f) ts
+                                               in (lc ++ rc, Map.union lm rm)
 
 class GSelectorInfo f where
-    getSelectorInfo :: f a -> ([SelectorInfo], Map TypeRep DatatypeInfo)
+    getSelectorInfo :: f a -> Set TypeRep -> ([SelectorInfo], Map TypeRep DatatypeInfo)
 
 instance (Selector a, GFieldInfo c) => GSelectorInfo (S1 a c) where
-    getSelectorInfo c@(M1 r) = let (t, m) = getFieldInfo r
-                                in ([SelectorInfo (Text.pack . selName $ c) (selSourceUnpackedness c) (selSourceStrictness c) (selDecidedStrictness c) t], m)
+    getSelectorInfo c@(M1 r) ts = let (t, m) = getFieldInfo r ts
+                                   in ([SelectorInfo (Text.pack . selName $ c) (selSourceUnpackedness c) (selSourceStrictness c) (selDecidedStrictness c) t], m)
 
 instance (GSelectorInfo a, GSelectorInfo b) => GSelectorInfo (a :*: b) where
-    getSelectorInfo (_ :: (a :*: b) f) = let (lt, lm) = getSelectorInfo (undefined :: a f)
-                                             (rt, rm) = getSelectorInfo (undefined :: b f)
-                                          in (lt ++ rt, Map.union lm rm)
+    getSelectorInfo (_ :: (a :*: b) f) ts = let (lt, lm) = getSelectorInfo (undefined :: a f) ts
+                                                (rt, rm) = getSelectorInfo (undefined :: b f) ts
+                                            in (lt ++ rt, Map.union lm rm)
 
 instance GSelectorInfo U1 where
-    getSelectorInfo _ = ([], Map.empty)
+    getSelectorInfo _ ts = ([], Map.empty)
 
 class GFieldInfo f where
-    getFieldInfo :: f a -> (TypeRep, Map TypeRep DatatypeInfo)
-
-{- List cons is causing infinite recursion -}
+    getFieldInfo :: f a -> Set TypeRep -> (TypeRep, Map TypeRep DatatypeInfo)
 
 instance (IsPrim c ~ p, MkTypeInfo p c, Typeable c) => GFieldInfo (K1 a c) where
-    getFieldInfo _ = (typeRep (Proxy :: Proxy c), mkTypeInfo (Proxy :: Proxy p) (Proxy :: Proxy c))
+    getFieldInfo _ ts = let t = typeRep (Proxy :: Proxy c)
+                         in (t, if Set.member t ts then Map.empty else mkTypeInfo (Proxy :: Proxy p) (Proxy :: Proxy c) ts)   
 
 instance GFieldInfo U1 where
-    getFieldInfo _ = (typeRep (Proxy :: Proxy ()), Map.empty)
+    getFieldInfo _ ts = (typeRep (Proxy :: Proxy ()), Map.empty)
