523c59161db4a5f16c51e3b981acd8cf2237416e
Micah Hahn <micah.s.hahn@gmail.com>
2019-02-17 22:29:19 -0600

    Use types instead of strings in tsTypeRep

--- src/Servant/TS.hs
+++ src/Servant/TS.hs
@@ -9,9 +9,9 @@
 
 module Servant.TS (
     tsForAPI,
-    TSType(..),
-    TypeContext(..),
-    TypeConstructor(..),
+    TsType(..),
+    TsContext(..),
+    TsTypeable(..),
     TypeScript(..)
 ) where
 
@@ -52,28 +52,38 @@ import Servant.Foreign
 -- Dummy type to parameterize instances
 data TypeScript
 
-instance (TSType a) => HasForeignType TypeScript (TypeContext Text) a where
-    typeFor _ _ p = tsType p
+instance (TsTypeable a) => HasForeignType TypeScript (TsContext TsType) a where
+    typeFor _ _ p = tsTypeRep p
 
-tsForAPI :: (HasForeign TypeScript (TypeContext Text) api, GenerateList (TypeContext Text) (Foreign (TypeContext Text) api)) => Proxy api -> Text
-tsForAPI api = writeEndpoints $ listFromAPI (Proxy :: Proxy TypeScript) (Proxy :: Proxy (TypeContext Text)) api
+tsForAPI :: (HasForeign TypeScript (TsContext TsType) api, GenerateList (TsContext TsType) (Foreign (TsContext TsType) api)) => Proxy api -> Text
+tsForAPI api = writeEndpoints $ listFromAPI (Proxy :: Proxy TypeScript) (Proxy :: Proxy (TsContext TsType)) api
 
 {- Using 'data' in jquery options potentially incorrect? -}
 
-writeEndpoint :: Req (TypeContext Text) -> TypeContext Text
+tsTypeName :: TsType -> Text 
+tsTypeName TsVoid = "void"
+tsTypeName TsNever = "never"
+tsTypeName TsBoolean = "boolean"
+tsTypeName TsNumber = "number"
+tsTypeName TsString = "string"
+tsTypeName (TsNullable t) = tsTypeName t <> "?"
+tsTypeName (TsRef t) = sanitizeTSName . Text.pack . show $ typeRepTyCon t 
+tsTypeName (TsArray t) = "Array<" <> tsTypeName t <> ">"
+
+writeEndpoint :: Req (TsContext TsType) -> TsContext Text
 writeEndpoint t = do
     let functionName = mconcat . map Text.toTitle . unFunctionName . _reqFuncName $ t
     let method = TE.decodeUtf8 $ _reqMethod t
-    successType <- maybe (return "void") id (_reqReturnType t)
-    captures <- sequence [type' >>= (\t -> return $ name <> ": " <> t) | Cap (Arg (PathSegment name) type') <- (unSegment <$> (_path . _reqUrl $ t))]
-    (bodyArg, bodyJQueryArg) <- maybe (return ([], [])) (fmap (\t -> (["$body: " <> t], [("data", "$body")]))) $ _reqBody t
+    successType <- maybe (return TsVoid) id (_reqReturnType t)
+    captures <- sequence [type' >>= (\t -> return $ name <> ": " <> tsTypeName t) | Cap (Arg (PathSegment name) type') <- (unSegment <$> (_path . _reqUrl $ t))]
+    (bodyArg, bodyJQueryArg) <- maybe (return ([], [])) (fmap (\t -> (["$body: " <> tsTypeName t], [("data", "$body")]))) $ _reqBody t
     
     q <- sequence $ (\a -> do
                         t <- _argType . _queryArgName $ a
                         return (unPathSegment . _argName . _queryArgName $ a, t)
                     ) <$> (_queryStr . _reqUrl $ t)
     
-    let queryArgs = if null q then [] else [("$query: {" <> Text.intercalate ", " ((\(l, r) -> l <> ": " <> r) <$> q) <> "}")]
+    let queryArgs = if null q then [] else [("$query: {" <> Text.intercalate ", " ((\(l, r) -> l <> ": " <> tsTypeName r) <$> q) <> "}")]
 
     let queryPrepare = if null q then ""
                                  else "\t\tlet $queryArgs : string[] = [];\n" <>
@@ -84,7 +94,7 @@ writeEndpoint t = do
     let url = "\"/\" + " <> (Text.intercalate "+ \"/\" + " (mapSegment . unSegment <$> (_path . _reqUrl $ t))) <>
               if null q then "" else " + $queryString"
 
-    let args = captures ++ queryArgs ++ bodyArg ++ ["onSuccess: (result: " <> successType <> ") => void", "onError: () => void"]
+    let args = captures ++ queryArgs ++ bodyArg ++ ["onSuccess: (result: " <> tsTypeName successType <> ") => void", "onError: () => void"]
     let jqueryArgs = [("url", url), ("success", "onSuccess"), ("error", "onError"), ("method", "\"" <> method <> "\"")] ++ bodyJQueryArg
     return $ "\texport function " <> functionName <> "(" <> Text.intercalate ", " args <> "): void\n" <>
              "\t{\n" <>
@@ -92,242 +102,228 @@ writeEndpoint t = do
              "\t\t$.ajax({\n\t\t\t" <> Text.intercalate ",\n\t\t\t" ((\(l, r) -> l <> ": " <> r) <$> jqueryArgs) <> "\n\t\t});\n" <>
              "\t}"
 
-    where mapSegment :: SegmentType (TypeContext Text) -> Text
+    where mapSegment :: SegmentType (TsContext TsType) -> Text
           mapSegment (Static (PathSegment s)) = "\"" <> s <> "\""
           mapSegment (Cap (Arg (PathSegment name) _)) = "encodeURIComponent(String(" <> name <> "))"
 
-writeEndpoints :: [Req (TypeContext Text)] -> Text
-writeEndpoints ts = let (TypeContext ts' m) = sequence (writeEndpoint <$> ts)
+          writeTsType :: TsType -> Text
+          writeTsType TsNever = "never"
+          writeTsType TsBoolean = "boolean"
+          writeTsType TsNumber = "number"
+          writeTsType TsString = "string"
+          writeTsType (TsUnion ts) = Text.intercalate " | " (writeTsType <$> ts)
+          writeTsType (TsNullable t) = (writeTsType t) <> " | null"
+
+writeEndpoints :: [Req (TsContext TsType)] -> Text
+writeEndpoints ts = let (TsContext ts' m) = sequence (writeEndpoint <$> ts)
                      in "namespace Ajax\n" <>
                         "{\n" <>
-                        Text.intercalate "\n\n" (Map.elems m) <> "\n" <>
+                        {- Text.intercalate "\n\n" (Map.elems m) <> "\n" <> -}
                         Text.intercalate "\n\n" ts' <> "\n" <>
                         "}"
 
-data TsType = TsBoolean
+data TsType = TsVoid
+            | TsNever
+            | TsBoolean
             | TsNumber
             | TsString
-            | TsUnion [(Text, TsType)]
+            | TsUnion [TsType]
             | TsNullable TsType
-            | TsObject [(Text, TsType)]
-            | TsTuple [TsType]
             | TsArray TsType
+            | TsObject Text [(Text, TsType)]
+            | TsTuple Text [TsType]
+            | TsRef TypeRep
     deriving (Show)
 
-data TypeConstructor = TypeConstructor
-    { datatype :: TypeRep
-    , constructor :: Text
-    } deriving (Eq, Ord, Show)
-
-data TypeContext a = TypeContext a (Map TypeConstructor Text)
+data TsContext a = TsContext a (Map TypeRep TsType)
     deriving (Show, Functor)
 
-instance Applicative TypeContext where
-    pure a = TypeContext a (Map.empty)
-    (<*>) (TypeContext f m) (TypeContext a m') = TypeContext (f a) (Map.union m m')
+instance Applicative TsContext where
+    pure a = TsContext a (Map.empty)
+    (<*>) (TsContext f m) (TsContext a m') = TsContext (f a) (Map.union m m')
 
-instance Monad TypeContext where
+instance Monad TsContext where
     return = pure
-
-    (>>=) (TypeContext a m) f = let (TypeContext a' m') = f a
-                                 in TypeContext a' (Map.union m m') 
+    (>>=) (TsContext a m) f = let (TsContext a' m') = f a
+                               in TsContext a' (Map.union m m')
 
 sanitizeTSName :: Text -> Text
 sanitizeTSName = Text.replace "'" ""
 
-class TSType a where
-    tsType :: Proxy a -> TypeContext Text
-    default tsType :: (Generic a, Typeable a, TSDatatype (Rep a)) => Proxy a -> TypeContext Text
-    tsType _ = tsDatatype (from (undefined :: a)) (typeRep (Proxy :: Proxy a))
+class TsTypeable a where
+    tsTypeRep :: Proxy a -> TsContext TsType
+    default tsTypeRep :: (Generic a, Typeable a, TsDatatype (Rep a)) => Proxy a -> TsContext TsType
+    tsTypeRep p = tsDatatype (from (undefined :: a)) (typeRep p)
 
-class TSDatatype a where
-    tsDatatype :: a p -> TypeRep -> TypeContext Text
+class TsDatatype a where
+    tsDatatype :: a p -> TypeRep -> TsContext TsType
 
-instance (Datatype a, TSConstructor c) => TSDatatype (D1 a c) where
-    tsDatatype c@(M1 r) t = tsConstructor r t False
+instance (Datatype a, TsConstructor c) => TsDatatype (D1 a c) where
+    tsDatatype c@(M1 r) t = do
+        cons <- tsConstructor r
+        let tsType = if length cons == 1 then head cons
+                                         else TsUnion cons
+        TsContext (TsRef t) (Map.insert t tsType Map.empty)
 
-class TSConstructor a where
-    tsConstructor :: a p -> TypeRep -> Bool -> TypeContext Text
+class TsConstructor a where
+    tsConstructor :: a p -> TsContext [TsType]
 
-instance (TSConstructor a, TSConstructor b) => TSConstructor (a :+: b) where
-    tsConstructor (_ :: (a :+: b) f) t _ = do
-        l <- tsConstructor (undefined :: a f) t True
-        r <- tsConstructor (undefined :: b f) t True
-        return $ l <> " | " <> r
+instance (TsConstructor a, TsConstructor b) => TsConstructor (a :+: b) where
+    tsConstructor (_ :: (a :+: b) f) = do
+        l <- (tsConstructor (undefined :: a f))
+        r <- (tsConstructor (undefined :: b f))
+        return $ l ++ r
                                          
-instance (Constructor a, TSSelector c) => TSConstructor (C1 a c) where
-    tsConstructor c@(M1 r) t isTagged = do
-        selectors <- tsSelector r
-        let con = TypeConstructor t (sanitizeTSName . Text.pack . conName $ c)
-        let interface = ("\tinterface " <> (sanitizeTSName . Text.pack . conName $ c) <> "\n" <>
-                         "\t{\n" <>
-                         (if isTagged then "\t\ttag: \"" <> (sanitizeTSName . Text.pack . conName $ c) <> "\";\n" else "") <>
-                         (if null selectors then "" else 
-                            if conIsRecord c then mconcat $ (\(l, r) -> "\t\t" <> l <> ": " <> r <> ";\n") <$> selectors 
-                                             else "\t\tcontents: [" <> Text.intercalate ", " (snd <$> selectors) <> "];" ) <>
-                         "\t}\n")
-        TypeContext (sanitizeTSName . Text.pack . conName $ c) (Map.insert con interface Map.empty)
+instance (Constructor a, TsSelector c) => TsConstructor (C1 a c) where
+    tsConstructor c@(M1 r) = do
+        sels <- tsSelector r
+        let n = sanitizeTSName . Text.pack . conName $ c
+        let tsType = if conIsRecord c then TsObject n sels else TsTuple n (snd <$> sels)
+        return [tsType]
 
-class TSSelector a where
-    tsSelector :: a p -> TypeContext [(Text, Text)]
+class TsSelector a where
+    tsSelector :: a p -> TsContext [(Text, TsType)]
 
-instance (TSSelector a, TSSelector b) => TSSelector (a :*: b) where
+instance (TsSelector a, TsSelector b) => TsSelector (a :*: b) where
     tsSelector (_ :: (a :*: b) f) = do
         l <- tsSelector (undefined :: a f)
         r <- tsSelector (undefined :: b f)
-        return (l ++ r)
+        return $ l ++ r
 
-instance (Selector a, TSType c) => TSSelector (S1 a (K1 b c)) where
-    tsSelector c@(M1 (K1 t)) = do
-        field <- tsType (Proxy :: Proxy c)
-        return [(Text.pack . selName $ c, field)]
+instance (Selector a, TsTypeable c) => TsSelector (S1 a (K1 b c)) where
+    tsSelector q@(M1 (K1 t)) = do
+        tsType <- tsTypeRep (Proxy :: Proxy c)
+        return [(Text.pack . selName $ q, tsType)]
 
-{- instance (TSType c) => TSSelector (K1 a c) where
-    tsSelector _ = undefined -}
-
-instance TSSelector U1 where
+instance TsSelector U1 where
     tsSelector _ = return []
 
-instance TSType Bool where
-    tsType _ = return "boolean"
+instance TsTypeable Bool where
+    tsTypeRep _ = return TsBoolean
 
-instance TSType Ordering where
-    tsType _ = return "string"
+instance TsTypeable Ordering where
+    tsTypeRep _ = return TsString
 
-instance TSType () where
-    tsType _ = return "Array<never>"
+instance TsTypeable () where
+    tsTypeRep _ = return $ TsArray TsNever
 
-instance TSType Char where
-    tsType _ = return "string"
+instance TsTypeable Char where
+    tsTypeRep _ = return TsString
 
-instance TSType Double where
-    tsType _ = return "number"
+instance TsTypeable Double where
+    tsTypeRep _ = return TsNumber
 
 {-
-instance TSType Number where
+instance TsType Number where
     tsType _ = "number"
 -}
 
-instance TSType Float where
-    tsType _ = return "number"
+instance TsTypeable Float where
+    tsTypeRep _ = return TsNumber
 
-instance (TSType a, Integral a) => TSType (Ratio a) where
-    tsType _ = return "number"
+instance (TsTypeable a, Integral a) => TsTypeable (Ratio a) where
+    tsTypeRep _ = return TsNumber
     
-instance (HasResolution a) => TSType (Fixed a) where
-    tsType _ = return "number"
+instance (HasResolution a) => TsTypeable (Fixed a) where
+    tsTypeRep _ = return TsNumber
 
-instance TSType Int where
-    tsType _ = return "number"
+instance TsTypeable Int where
+    tsTypeRep _ = return TsNumber
 
-instance TSType Integer where 
-    tsType _ = return "number"
+instance TsTypeable Integer where 
+    tsTypeRep _ = return TsNumber
 
-instance TSType Natural where
-    tsType _ = return "number"
+instance TsTypeable Natural where
+    tsTypeRep _ = return TsNumber
 
-instance TSType Int8 where
-    tsType _ = return "number"
+instance TsTypeable Int8 where
+    tsTypeRep _ = return TsNumber
 
-instance TSType Int16 where
-    tsType _ = return "number"
+instance TsTypeable Int16 where
+    tsTypeRep _ = return TsNumber
 
-instance TSType Int32 where
-    tsType _ = return "number"
+instance TsTypeable Int32 where
+    tsTypeRep _ = return TsNumber
 
-instance TSType Int64 where
-    tsType _ = return "number"
+instance TsTypeable Int64 where
+    tsTypeRep _ = return TsNumber
 
-instance TSType Word where
-    tsType _ = return "number"
+instance TsTypeable Word where
+    tsTypeRep _ = return TsNumber
 
-instance TSType Word8 where
-    tsType _ = return "number"
+instance TsTypeable Word8 where
+    tsTypeRep _ = return TsNumber
 
-instance TSType Word16 where
-    tsType _ = return "number"
+instance TsTypeable Word16 where
+    tsTypeRep _ = return TsNumber
 
-instance TSType Word32 where
-    tsType _ = return "number"
+instance TsTypeable Word32 where
+    tsTypeRep _ = return TsNumber
 
-instance TSType Word64 where
-    tsType _ = return "number"
+instance TsTypeable Word64 where
+    tsTypeRep _ = return TsNumber
 
-instance TSType CTime where
-    tsType _ = return "number"
+instance TsTypeable CTime where
+    tsTypeRep _ = return TsNumber
 
-instance TSType Text where
-    tsType _ = return "string"
+instance TsTypeable Text where
+    tsTypeRep _ = return TsString
 
-instance TSType LT.Text where
-    tsType _ = return "string"
+instance TsTypeable LT.Text where
+    tsTypeRep _ = return TsString
 
-instance TSType Version where
-    tsType _ = return "string"
+instance TsTypeable Version where
+    tsTypeRep _ = return TsString
 
-instance (TSType a) => TSType (Maybe a) where
-    tsType _ = do
-        ts <- tsType (Proxy :: Proxy a)
-        return $ ts <> " | null"
+instance (TsTypeable a) => TsTypeable (Maybe a) where
+    tsTypeRep _ = TsNullable <$> (tsTypeRep (Proxy :: Proxy a))
 
-instance (TSType a) => TSType [a] where
-    tsType _ = do
-        ts <- tsType (Proxy :: Proxy a)
-        return $ "Array<" <> ts <> ">"
+instance (TsTypeable a) => TsTypeable [a] where
+    tsTypeRep _ = TsArray <$> (tsTypeRep (Proxy :: Proxy a))
 
-instance (TSType a) => TSType (NonEmpty a) where
-    tsType _ = do
-        ts <- tsType (Proxy :: Proxy a)
-        return $ "Array<" <> ts <> ">"
+instance (TsTypeable a) => TsTypeable (NonEmpty a) where
+    tsTypeRep _ = TsArray <$> (tsTypeRep (Proxy :: Proxy a))
 
 {-
-instance TSType Scientific where
+instance TsType Scientific where
     tsType _ = "number"
 -}
 
 {-
-instance (TSType a) => TSType (DList a) where
+instance (TsType a) => TsType (DList a) where
     tsType _ = "Array<" <> tsType (Proxy :: Proxy a) <> ">"
 -}
 
-instance (TSType a) => TSType (Identity a) where
-    tsType _ = tsType (Proxy :: Proxy a)
+instance (TsTypeable a) => TsTypeable (Identity a) where
+    tsTypeRep _ = tsTypeRep (Proxy :: Proxy a)
 
 {-
-instance (TSType a, TSType b) => TSType (Compose f a b) where
+instance (TsType a, TsType b) => TsType (Compose f a b) where
     tsType _ = 
 -}
 
 {-
-instance (TSType a, TSType b) => TSType (Product f a b) where
+instance (TsType a, TsType b) => TsType (Product f a b) where
     tsType _ = 
 -}
 
-instance (TSType a) => TSType (Seq a) where
-    tsType _ = do
-        ts <- tsType (Proxy :: Proxy a)
-        return $ "Array<" <> ts <> ">"
+instance (TsTypeable a) => TsTypeable (Seq a) where
+    tsTypeRep _ = TsArray <$> (tsTypeRep (Proxy :: Proxy a))
 
-instance (TSType a) => TSType (Set a) where
-    tsType _ = do
-        ts <- tsType (Proxy :: Proxy a)
-        return $ "Array<" <> ts <> ">"
+instance (TsTypeable a) => TsTypeable (Set a) where
+    tsTypeRep _ = TsArray <$> (tsTypeRep (Proxy :: Proxy a))
 
-instance TSType IntSet where
-    tsType _ = do
-        ts <- tsType (Proxy :: Proxy Int)
-        return $ "Array<" <> ts <> ">"
+instance TsTypeable IntSet where
+    tsTypeRep _ = return $ TsArray TsNumber
 
-{- instance TSType IntMap where -}
+{- instance TsType IntMap where -}
 
-{- instance TSType Tree where -}
+{- instance TsType Tree where -}
 
-{- instance TSType UUID -}
+{- instance TsType UUID -}
 
-instance (TSType a) => TSType (V.Vector a) where
-    tsType _ = do
-        ts <- tsType (Proxy :: Proxy a)
-        return $ "Array<" <> ts <> ">"
+instance (TsTypeable a) => TsTypeable (V.Vector a) where
+    tsTypeRep _ = TsArray <$> (tsTypeRep (Proxy :: Proxy a))
 
 {- instance VS.Vector -}
 
@@ -351,8 +347,8 @@ instance (TSType a) => TSType (V.Vector a) where
 
 {- instance ToJSON TimeOfDay where -}
 
-instance TSType LocalTime where
-    tsType _ = return "string"
+instance TsTypeable LocalTime where
+    tsTypeRep _ = return TsString
 
 {- instance ToJSON ZonedTime where -}
 
---
+++ src/Servant/Test.hs
@@ -0,0 +1,176 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module Servant.Test where
+
+import Control.Monad.IO.Class
+import Data.ByteString.Lazy as L
+import Data.Text (Text)
+import Data.Text.Encoding as TE
+import qualified Data.Text as Text
+import Text.Read
+import Data.Proxy
+import Lucid
+import Network.Wai.Handler.Warp
+import Servant
+import Servant.HTML.Lucid
+
+import Data.Maybe (isNothing)
+
+import GHC.Generics
+
+import Data.Time
+import Data.Time.Format
+
+import Data.Functor.Identity
+import Data.Functor.Const
+
+import Web.FormUrlEncoded
+
+import Pdf.Content.Processor
+
+import Debug.Trace
+
+import Pdf
+import Servant.JS
+
+import Data.Aeson
+
+import Servant.TS
+import qualified Data.Text.IO as TIO
+
+import Data.Typeable
+
+type InnerApi = "receipts" :> Get '[JSON] [FullReceipt]
+           :<|> "receipt" :> Capture "x" Int :> Get '[JSON] Int
+           :<|> "receipts" :> "query" :> QueryParam "x" Int :> QueryParam "y" Int :> Post '[JSON] Int
+           :<|> "receiptt" :> Capture "x" Int :> QueryParam "x" (Maybe Int) :> Get '[JSON] Int
+           :<|> "receipt" :> "body" :> ReqBody '[JSON] FullReceipt :> Get '[JSON] Int
+
+handler :: Maybe (FullReceipt' Identity) -> Handler Int
+handler _ = return 7
+
+h__ :: Server InnerApi
+h__ = return undefined
+ :<|> return undefined
+ :<|> return undefined
+ :<|> return undefined
+ :<|> return undefined
+
+h = serve (Proxy :: Proxy InnerApi) h__
+
+g = do
+    let t = tsForAPI (Proxy :: Proxy InnerApi)
+    TIO.writeFile "C:/Users/Micah/Source/Austerity/build/Endpoints.ts" t
+
+type AusterityHome = "home" :> Get '[HTML] (Html ())
+type AusterityReceiptsNewGet = "receipts" :> "new" :> Get '[HTML] (Html ())
+type AusterityReceiptsNewPost = "receipts" :> "new" :> ReqBody '[FormUrlEncoded] FullReceiptForm :> Post '[HTML] (Html ())
+
+type AusterityJSTest = "receipts" :> "new" :> ReqBody '[JSON] FullReceiptForm :> Post '[JSON] FullReceiptError
+
+type AusterityApi = AusterityHome
+               :<|> AusterityReceiptsNewGet
+               :<|> AusterityReceiptsNewPost
+               :<|> "static" :> Raw
+
+home :: Html ()
+home = do
+    doctypehtml_ $ do
+        html_ $ do
+            head_ $ do
+                title_ "Austerity"
+            body_ $ do
+                p_ "Welcome to Austerity"
+                form_ [action_ "import", method_ "post", enctype_ "multipart/form-data"] $ do
+                    input_ [type_ "file", name_ "file"]
+                    input_ [type_ "submit", value_ "Import"]
+
+defaultFullReceiptError :: FullReceiptError
+defaultFullReceiptError = FullReceipt' 
+    { date = Const ("", Nothing)
+    , vendor = Const ("", Nothing)
+    , items = []
+    , amount = Const ("", Nothing)
+    }
+
+type family Props a where
+    Props FullReceipt = FullReceiptProps
+
+data FullReceiptProps = FullReceiptProps
+    { vendors :: [VendorProps]
+    } deriving (Generic)
+ 
+deriving instance ToJSON FullReceiptProps
+
+data VendorProps = VendorProps
+    { vendorId :: Text
+    , vendorName :: Text
+    } deriving (Generic)
+
+deriving instance ToJSON VendorProps
+    
+dateFormat :: String
+dateFormat = "%m/%d/%Y %I:%M %p"
+
+validateFullReceipt :: FullReceiptForm -> Either FullReceiptError FullReceipt
+validateFullReceipt form = let mdate = parseTimeM False defaultTimeLocale dateFormat (Text.unpack . getConst . date $ form) :: Maybe LocalTime
+                               mvendor = readMaybe (Text.unpack . getConst . vendor $ form) :: Maybe Int
+                               mamount = readMaybe (Text.unpack . getConst . amount $ form) :: Maybe Double
+                               receipt = FullReceipt' <$> (Identity <$> mdate) <*> (Identity <$> mvendor) <*> (Just [Identity 1, Identity 2]) <*> (Identity <$> mamount)
+                           in  trace (show form) $ case receipt of
+                               Just r -> Right r
+                               Nothing -> Left $ FullReceipt'
+                                            { date = Const (getConst $ date form, maybe (Just "The date must be the format 01/01/2000 12:00 AM") (const Nothing) mdate)
+                                            , vendor = Const (getConst $ vendor form, maybe (Just "You must choose a vendor") (const Nothing) mvendor)
+                                            , items = [Const ("1", Nothing)]
+                                            , amount = Const (getConst $ amount form, maybe (Just "Must be in the format 100.00") (const Nothing) mamount)
+                                            } 
+
+data FullReceipt' a = FullReceipt'
+    { date :: a LocalTime
+    , vendor :: a Int
+    , items :: [a Int]
+    , amount :: a Double
+    }
+
+type FullReceipt = FullReceipt' Identity
+deriving instance Show FullReceipt
+deriving instance Generic FullReceipt
+deriving instance TsTypeable FullReceipt
+deriving instance ToJSON FullReceipt
+deriving instance FromJSON FullReceipt
+
+{- The type to be marshalled from the body of an HTTP request -}
+type FullReceiptForm = FullReceipt' (Const Text)
+deriving instance Show FullReceiptForm
+deriving instance Generic FullReceiptForm
+deriving instance FromForm FullReceiptForm
+
+{-  -}
+type FullReceiptError = FullReceipt' (Const (Text, Maybe Text))
+deriving instance Show FullReceiptError 
+
+instance (FromHttpApiData a) => FromHttpApiData (Const a b) where
+    parseUrlPiece t = Const <$> parseUrlPiece t
+
+doubleText = Text.pack . show
+
+renderTextBox :: Double -> TextBox -> Html ()
+renderTextBox pageHeight (TextBox (Rect l t w h) text) = do
+    div_ [class_ "textbox", style_ ("left: " <> doubleText l <> "px; top: " <> doubleText (pageHeight - t) <> "px; font-size: " <> doubleText h <> "px; width: " <> doubleText w <> "px; height: " <> doubleText h <> "px;")] (toHtml text)
+
+renderPage :: TextPage -> Html ()
+renderPage (TextPage (Rect _ _ width height) bs) = do
+    div_ [class_ "page", style_ ("width: " <> doubleText width <> "px; height: " <> doubleText height <> "px;")] $ do
+        mapM_ (renderTextBox height) bs
+
+renderPdf :: [TextPage] -> Html ()
+renderPdf ps =  mapM_ renderPage ps
